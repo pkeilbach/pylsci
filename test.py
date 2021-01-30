ebc49bdc8f793e056b6181b42e1cfac4d5f34db5
Pascal Keilbach <pascal.keilbach@cyber.bka.de>
2021-01-30 19:23:24 +0100

    banned matplotlib from test scriipt

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from pylsci import Lsci
-from util import stack_images, show_image
+from util import stack_images, show_image, read_image
 
 lsci = Lsci()
 
 print('temporal LSCI ...')
-speckle_imgs = plt.imread('img/temporal.png')
+speckle_imgs = read_image('img/temporal.png')
 speckle_img_stack = stack_images(speckle_imgs)
 print(speckle_img_stack.shape)
 t_lsci = lsci.temporal_contrast(speckle_img_stack)
@@ -12,7 +12,7 @@ show_image(t_lsci)
 print(t_lsci.shape)
 
 print('spatio-temporal LSCI ...')
-speckle_imgs = plt.imread('img/temporal.png')
+speckle_imgs = read_image('img/temporal.png')
 speckle_img_stack = stack_images(speckle_imgs)
 print(speckle_img_stack.shape)
 st_lsci = lsci.spatio_temporal_contrast(speckle_img_stack)
@@ -20,7 +20,7 @@ show_image(st_lsci)
 print(st_lsci.shape)
 
 print('spatial LSCI ...')
-speckle_img = plt.imread('img/spatial.tif')
+speckle_img = read_image('img/spatial.tif')
 print(speckle_img.shape)
 s_lsci = lsci.spatial_contrast(speckle_img)
 show_image(s_lsci)
