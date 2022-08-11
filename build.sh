8d6193c756fcafd0355921ba51a5ddd13cf340fc
pkeilbach <dev@pk17.org>
2022-08-11 22:14:47 +0200

    change project to src layout

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 rm -rf build/ dist/ py_redis.egg-info/
 python setup.py sdist bdist_wheel
-twine upload dist/*
+# twine upload dist/*
+
+twine upload --repository testpypi dist/*
--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 import setuptools
 
-# build
-# python setup.py sdist bdist_wheel
-# twine upload --repository testpypi dist/* or twine upload dist/*
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+
 setuptools.setup(
     name="pylsci",
-    version="1.0.3",
+    version="1.0.4",
     author="Pascal Keilbach",
     author_email="dev@pk17.org",
     description="Python Package for Laser Speckle Contrast Imaging",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pkeilbach/pylsci",
-    packages=setuptools.find_packages(),
+    packages=["pylsci"],
+    package_dir={"": "src"},
     install_requires=["numpy"],
     classifiers=[
         "Programming Language :: Python :: 3",
