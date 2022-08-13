6c387c0ab221a96e3c066f030d178bc3413d839f
pkeilbach <dev@pk17.org>
2022-08-13 10:32:21 +0200

    setup.py for editable installs

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-import setuptools
+from setuptools import setup
 
-
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-
-setuptools.setup(
-    name="pylsci",
-    version="1.0.4",
-    author="Pascal Keilbach",
-    author_email="dev@pk17.org",
-    description="Python Package for Laser Speckle Contrast Imaging",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pkeilbach/pylsci",
-    packages=["pylsci"],
-    package_dir={"": "src"},
-    install_requires=["numpy"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-)
+setup()
