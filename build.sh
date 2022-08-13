8a0711bfaa3e96e236fcf7d11a14292e5221ffa3
pkeilbach <28002199+pkeilbach@users.noreply.github.com>
2022-08-13 10:46:06 +0200

    Iss/26 pyproject toml (#27)
    * add pyproject.toml
    
    * setup.py for editable installs
    
    * mv to test dir
    
    * rm not needed files

--- build.sh
+++
@@ -1,5 +0,0 @@
-rm -rf build/ dist/ py_redis.egg-info/
-python setup.py sdist bdist_wheel
-twine upload dist/*
-
-# twine upload --repository testpypi dist/*
--- setup.py
+++ setup.py
@@ -1,26 +1,9 @@
-import setuptools
+"""
+this is only for editable installs
 
+https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html#
+"""
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+from setuptools import setup
 
-
-setuptools.setup(
-    name="pylsci",
-    version="1.0.4",
-    author="Pascal Keilbach",
-    author_email="dev@pk17.org",
-    description="Python Package for Laser Speckle Contrast Imaging",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pkeilbach/pylsci",
-    packages=["pylsci"],
-    package_dir={"": "src"},
-    install_requires=["numpy"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-)
+setup()
